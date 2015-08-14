aebfd0f1dd900d7620b329572d7f236a74d2f588
Vyacheslav Zenkevich <vyacheslav.zenkevich@teknavo.com>
2015-08-14 17:50:26 +0300

    Fixed product div height issue. Working beautify search form but thta is not yet complete

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
       fulltext  params[:query]
   	  #order_by :title, :desc
   	  #order_by :description, :desc
-      paginate :page => params[:page], :per_page => 5
+      paginate :page => params[:page], :per_page => 12
     end.results
     @total_pages = @products.total_pages
   end
