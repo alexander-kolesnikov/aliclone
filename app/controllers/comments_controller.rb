7711c7b2f0fdb94b06c8f6b7a9890cf4f4a1b299
Vyacheslav Zenkevich <vyacheslav.zenkevich@teknavo.com>
2015-08-20 12:32:08 +0300

    Fixed token issue and a validation issue

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
 
   def create
-    Product.find(params[:product_id]).comments.create(contents: params[:comment][:contents])
+    Product.find(params[:product_id]).comments.create(contents: params[:comment][:contents], author: current_user)
   	redirect_to :back
   end
 end
